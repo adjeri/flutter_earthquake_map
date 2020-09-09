10201fcb560611c0f96d2bba219956863b8545f6
Adjéri SABI <kod047@gmail.com>
2020-09-10 03:28:54 +0900

    types helper

---
+++ lib/ui/quake_map_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class QuakesApp extends StatefulWidget {
+  @override
+  _QuakesAppState createState() => _QuakesAppState();
+}
+
+class _QuakesAppState extends State<QuakesApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
