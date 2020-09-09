f5c4602e100c51469b688261c37ffe4ea438a143
Adjéri SABI <kod047@gmail.com>
2020-09-10 05:43:16 +0900

    setup markers

--- lib/ui/quake_map_app.dart
+++ lib/ui/quake_map_app.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map/model/quake.dart';
 import 'package:flutter_map/network/network.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class QuakesApp extends StatefulWidget {
   @override
@@ -10,18 +13,79 @@ class QuakesApp extends StatefulWidget {
 
 class _QuakesAppState extends State<QuakesApp> {
   Future<Quake> _quakesData;
+  Completer<GoogleMapController> _controller = Completer();
+  List<Marker> _markerList = <Marker>[];
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _quakesData = Network().getAllQuakes();
-    _quakesData.then((values) => {
-      print("Place: ${values.features[0].properties.place}")
-    });
+    _quakesData.then(
+            (values) =>
+        {
+          print("Place: ${values.features[0].properties.place}")
+        });
   }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          _buildGoogleMap(context),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            findQuakes();
+          },
+          label: Text("Find quakes")),
+    );
+  }
+
+  Widget _buildGoogleMap(BuildContext context) {
+    return Container(
+      width: MediaQuery
+          .of(context)
+          .size
+          .width,
+      height: MediaQuery
+          .of(context)
+          .size
+          .height,
+      child: GoogleMap(
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        initialCameraPosition:
+        CameraPosition(target: LatLng(36.1083331, -117.8608333), zoom: 3),
+      ),
+    );
+  }
+
+  void findQuakes() {
+    setState(() {
+      _markerList.clear();
+      _handleResponse();
+    });
+  }
+
+  void _handleResponse() {
+    setState(() {
+      _quakesData.then((quakes) =>
+      {
+        quakes.features.forEach((quake) =>
+        {
+          _markerList.add(Marker(markerId: MarkerId(quake.id),
+              infoWindow: InfoWindow(title: quake.properties.mag.toString()),
+              icon: BitmapDescriptor.defaultMarkerWithHue(
+                  BitmapDescriptor.hueBlue),
+              position: LatLng(quake.geometry.coordinates[1].toDouble(),
+                  quake.geometry.coordinates[0]),
+              onTap: () {}),)
+        })
+      });
+    });
   }
 }
